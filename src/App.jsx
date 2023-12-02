857b6862a2f2083bc8ea9eb57f1ab7edf768c4a3
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-02 14:36:58 +0700

    fix: make style properly

--- src/App.jsx
+++ src/App.jsx
@@ -20,27 +20,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <div className="flex mb-5">
+        <a
+          className="flex flex-1 justify-center"
+          href="https://vitejs.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a
+          className="flex flex-1 justify-center"
+          href="https://react.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="mb-3" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs mb-5">
         Click on the Vite and React logos to learn more
       </p>
-      <div className="telegram-login-widget" ref={telegramWrapperRef}></div>
+      <div
+        className="telegram-login-widget flex justify-center"
+        ref={telegramWrapperRef}
+      ></div>
     </>
   );
 }
