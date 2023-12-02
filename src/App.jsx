a70ae34f4a9f095429ef191a69e6daf283013598
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-02 15:10:04 +0700

    feat: add style to card

--- src/App.jsx
+++ src/App.jsx
@@ -70,19 +70,21 @@ function App() {
         className="telegram-login-widget flex justify-center mb-3"
         ref={telegramWrapperRef}
       ></div>
-      {id !== "" && firstName !== "" && lastName !== "" ? (
+      {id || firstName || lastName ? (
         <Card className="max-w-sm">
-          <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <h5 className="text-xl text-left font-bold tracking-tight text-gray-900 dark:text-white">
             Data from Telegram redirecting
           </h5>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            ID: {searchParams.get("id")}
+          <p className="font-normal text-left text-gray-700 dark:text-gray-400">
+            <span className="font-bold">ID: </span> {id}
           </p>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            First name: {searchParams.get("first_name")}
+          <p className="font-normal text-left text-gray-700 dark:text-gray-400">
+            <span className="font-bold">First name: </span>
+            {firstName}
           </p>
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            Last name: {searchParams.get("last_name")}
+          <p className="font-normal text-left text-gray-700 dark:text-gray-400">
+            <span className="font-bold">Last name: </span>
+            {lastName}
           </p>
         </Card>
       ) : (
